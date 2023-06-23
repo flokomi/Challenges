289da37e90da49580bdc7d39d4d7eb5da0598a0b
Florian Miller <flokomi@web.de>
2023-06-23 13:32:03 +0200

    react-developer-profile-card added skills

--- react-developer-profile-card/src/index.js
+++ react-developer-profile-card/src/index.js
@@ -8,7 +8,13 @@ function App() {
       <Avatar />
       <div className="data">
         <Intro />
-        <SkillList />
+        <SkillList
+          skillOne={{ text: "HTML & CSS🌈", bgColor: "MediumOrchid" }}
+          skillTwo={{ text: "Ruby on Rails💎", bgColor: "DarkOrange" }}
+          skillThree={{ text: "JavaScript🚩", bgColor: "DeepSkyBlue" }}
+          skillFour={{ text: "Webdesign👏", bgColor: "DeepPink" }}
+          skillFive={{ text: "Git & Github😎", bgColor: "LimeGreen" }}
+        />
       </div>
     </div>
   );
@@ -25,11 +31,54 @@ function Avatar() {
 }
 
 function Intro() {
-  return <h1>Florian Miller</h1>;
+  return (
+    <div>
+      <h1>Florian Miller</h1>
+      <p>
+        Full-Stack web developper. When not coding then surfscating or enjoying
+        to cook and eat great food. Always happy to see the eyes of my kids.
+      </p>
+    </div>
+  );
 }
 
-function SkillList() {
-  return <p>ich</p>;
+function SkillList(props) {
+  console.log(props);
+
+  return (
+    <li className="skill-list">
+      <div
+        className="skill"
+        style={{ backgroundColor: `${props.skillOne.bgColor}` }}
+      >
+        <span>{props.skillOne.text}</span>
+      </div>
+      <div
+        className="skill"
+        style={{ backgroundColor: `${props.skillTwo.bgColor}` }}
+      >
+        <span>{props.skillTwo.text}</span>
+      </div>
+      <div
+        className="skill"
+        style={{ backgroundColor: `${props.skillThree.bgColor}` }}
+      >
+        <span>{props.skillThree.text}</span>
+      </div>
+      <div
+        className="skill"
+        style={{ backgroundColor: `${props.skillFour.bgColor}` }}
+      >
+        <span>{props.skillFour.text}</span>
+      </div>
+      <div
+        className="skill"
+        style={{ backgroundColor: `${props.skillFive.bgColor}` }}
+      >
+        <span>{props.skillFive.text}</span>
+      </div>
+    </li>
+  );
 }
 
 const root = ReactDom.createRoot(document.getElementById("root"));
